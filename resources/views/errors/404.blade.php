e6153d8d89d9a868bbcca7e954a443bf864347e0
EvanSC <evangundala@gmail.com>
2025-02-09 21:10:02 +0700

    tambah custom not found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-layout-content class="w-full sm:w-[75%] mx-4 sm:mx-0 p-3 sm:p-4 xl:p-6 rounded-lg">
+        <div class="flex justify-center items-center">
+            <div class="text-center">
+                <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNWdxeXNnNHR0b2s0eDR4YXd3b3N2N3FhNHdqd3dnMzZnZ3RmbzQzOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ma7VlDSlty3EA/giphy.gif" alt="404" class="mx-auto">
+                <p class="text-sm sm:text-base lg:text-lg mt-2">Sorry the page you are looking for is not found</p>
+            </div>
+        </div>
+    </x-layout-content>
+</x-app-layout>
